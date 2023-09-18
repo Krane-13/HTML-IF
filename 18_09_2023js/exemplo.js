85014f3add050ba31682d94908fd2994505ce803
Krane-13 <83793463+Krane-13@users.noreply.github.com>
2023-09-18 11:10:50 -0300

    Add files via upload

---
+++ 18_09_2023js/exemplo.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    // if (confirm("Deseja salvar as informações?")){
+    //     alert("Informação salvas com sucesso");
+    // }else{
+    //     alert("Inserção cancelada");
+    // } Função text() -> get pega o conteudo textual ->set mudar o conteudo textual
+
+    paragrafo = $("p").text(); // obtanção de conteudo textual
+    // alert(paragrafo);
+    // $("p").text("Mensagem trocada"); // mudança de conteudo textual
+    $("#especial").text("Mensagem trocada"); // mudança de conteudo textual
+
+})
